a73e55acd351a64cf80f9d693266e346b5b7d9b2
David Santos <david99-31@hotmail.com>
2023-12-08 12:19:23 -0500

    Metodo multiplicar funciona correctamente

--- src/com/krakedev/TestCalculadora.java
+++ src/com/krakedev/TestCalculadora.java
@@ -8,15 +8,18 @@ public class TestCalculadora {
 		int resultadoSuma;
 		double resultadoResta;
 		double resultadoMultiplicar;
+		double resultadoDividir;
 		
 		resultadoSuma=calcu.sumar(5, 3);
 		resultadoResta=calcu.restar(5, 3);
 		resultadoMultiplicar=calcu.multiplicar(10, 5);
+		resultadoDividir=calcu.dividir(10, 2);
 		
 		
 		System.out.println("El resultado de 5 + 3 es: "+resultadoSuma);
 		System.out.println("El resultado de 5 - 3 es: "+resultadoResta);
 		System.out.println("El resultado de 10 * 5 es: "+resultadoMultiplicar);
+		System.out.println("El resultado de 10/2 es: "+resultadoDividir);
 	}
 
 }
