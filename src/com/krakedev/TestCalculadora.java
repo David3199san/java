fb8624b6f0b29f1f375faf9a6227968836bbf5d9
David Santos <david99-31@hotmail.com>
2023-12-08 12:21:32 -0500

    Metodo promediar funciona correctamente

--- src/com/krakedev/TestCalculadora.java
+++ src/com/krakedev/TestCalculadora.java
@@ -9,17 +9,20 @@ public class TestCalculadora {
 		double resultadoResta;
 		double resultadoMultiplicar;
 		double resultadoDividir;
+		double resultadoPromediar;
 		
 		resultadoSuma=calcu.sumar(5, 3);
 		resultadoResta=calcu.restar(5, 3);
 		resultadoMultiplicar=calcu.multiplicar(10, 5);
 		resultadoDividir=calcu.dividir(10, 2);
+		resultadoPromediar=calcu.promediar(10, 7, 8);
 		
 		
 		System.out.println("El resultado de 5 + 3 es: "+resultadoSuma);
 		System.out.println("El resultado de 5 - 3 es: "+resultadoResta);
 		System.out.println("El resultado de 10 * 5 es: "+resultadoMultiplicar);
 		System.out.println("El resultado de 10/2 es: "+resultadoDividir);
+		System.out.println("El promedio de 10, 7, 8 es: "+resultadoPromediar);
 	}
 
 }
